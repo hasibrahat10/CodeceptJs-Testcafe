8917d8b989e4ea9881f8294a59e1a514980a5b25
Md Hasibul Hasan Rahat <hasibrahat10@gmail.com>
2021-04-25 14:20:40 +0600

    Login test

--- example_test.js
+++ example_test.js
@@ -1,44 +1,13 @@
 Feature('example');
 
-Scenario('test something', ({ I }) => {
-    I.amOnPage('https://www.example.com')
-    I.wait(1)
-    I.waitForElement('h1')
-    I.see('example')
-    I.dontSeeElement('.login-form')
-    I.seeInCurrentUrl("example.com")
+Scenario('Login Test', ({ I }) => {
+    I.amOnPage('http://zero.webappsecurity.com/login.html')
+    I.waitForElement("#login_form")
+    I.fillField('#user_login', 'invalid username')
+    I.fillField('#user_password', 'invalid userpassword')
+    I.click("input[type='submit']")
+    I.waitForText('Login and/or password are wrong')
 });
 
-
-Scenario('test item two', ({ I }) => {
-    I.amOnPage('https://www.example.com')
-    I.wait(1)
-    I.dontSeeElement('.login-form')
-    I.seeInCurrentUrl("example.com")
-});
-
-Scenario('test item three', ({ I }) => {
-    I.amOnPage('https://www.example.com')
-    I.wait(1)
-   
-    I.seeInCurrentUrl("example.com")
-});
-
-
-Scenario('Full page screen shot', ({ I }) => {
-    I.amOnPage('https://www.example.com')
-    I.wait(1)
-    I.seeInCurrentUrl("example.com")
-    I.saveScreenshot('test.png', true)
-});
-
-
-Scenario('single element screenshot', ({ I }) => {
-    I.amOnPage('https://www.example.com')
-    I.wait(1)
-    I.seeInCurrentUrl("example.com")
-    I.saveElementScreenshot('h1', 'element.png')
-   
-});
 
 
